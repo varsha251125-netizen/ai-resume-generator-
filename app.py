89d75f57132999acb5d960b4ebd59acf29735b5d
varsha251125-netizen <varsha251125@gmail.com>
2026-02-25 19:12:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-import os
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 from flask import Flask, render_template, request, send_file
 from dotenv import load_dotenv
 from openai import OpenAI
